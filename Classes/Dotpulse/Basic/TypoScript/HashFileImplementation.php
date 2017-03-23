86249a31d2c1456629c247fb34d85717786a57b2
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2017-03-23 15:57:35 +0100

    imported 0.1.22 from 2016-12-15

---
+++ Classes/Dotpulse/Basic/TypoScript/HashFileImplementation.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dotpulse\Basic\TypoScript;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject;
+use TYPO3\Flow\Utility;
+
+class HashFileImplementation extends AbstractTypoScriptObject
+{
+    public function evaluate()
+    {
+        $filePath = $this->tsValue('file');
+        $fileContent = Utility\Files::getFileContents($filePath);
+        return md5($fileContent);
+    }
+}
