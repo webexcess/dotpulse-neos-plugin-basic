24cd74accc740e2222408cdeab36c451049cf94d
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2017-03-23 15:57:29 +0100

    imported 0.1.3 from 2016-08-27

--- Resources/Private/Components/GetType/index.js
+++ Resources/Private/Components/GetType/index.js
@@ -1,6 +1,6 @@
 // Get type of variable
 
-let possibleTypes = 'boolean number string function array date regexp object error symbol'.split(' ');
+let possibleTypes = 'Boolean Number String Function Array Date RegExp Object Error Symbol'.split(' ');
 
 export default function(variable) {
 	let type = (typeof variable).toLowerCase();
@@ -12,14 +12,14 @@ export default function(variable) {
 	if (variable == null || checkNaN()) {
 		return String(variable);
 	} else if (type === 'object' || type === 'function') {
-		let constructorType = variable.constructor.toString().toLowerCase();
+		let constructorType = variable.constructor.toString();
 		for (let index in possibleTypes) {
 			let possibleType = possibleTypes[index];
 			if (constructorType.includes(possibleType)) {
 				if (checkNaN(possibleType)) {
 					return String(variable);
 				}
-				return possibleType;
+				return possibleType.toLowerCase();;
 			}
 		}
 		return 'object';
